e67db987d41f39ec431b44964aca8683b64e9c86
harsh johar <hpsj3112@gmail.com>
2022-12-04 19:26:26 +0530

    few changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,9 @@ const Home: NextPage = () => {
     return (
         <div className="flex min-h-screen flex-col items-center justify-center py-2">
             <Head>
-                <title>Pec ACM Intro</title>
+                <title>PEC ACM</title>
                 <link rel="icon" href="/favicon.ico" />
+                <meta name="description" content="Welcome to PEC ACM CSS." />
             </Head>
 
             <section className="text-gray-600 body-font">
@@ -21,12 +22,20 @@ const Home: NextPage = () => {
                         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
                             Welcome to PEC ACM CSS
                         </h1>
-                        <p className="mb-8 leading-relaxed">A growing community of Computer Science enthusiasts. Our mission is to be the most impactful ACM student chapter. We are a 600+ strong community of Computer Science enthusiasts working together to advance Computing as a Science and Profession. We are part of the official ACM Student Chapter – PEC ACM. Our base camp is at Punjab Engineering College, Chandigarh, IN.</p>
+                        <p className="mb-8 leading-relaxed">
+                            A growing community of Computer Science enthusiasts.
+                            Our mission is to be the most impactful ACM student
+                            chapter. We are a 600+ strong community of Computer
+                            Science enthusiasts working together to advance
+                            Computing as a Science and Profession. We are part
+                            of the official ACM Student Chapter - PEC ACM. Our
+                            base camp is at Punjab Engineering College,
+                            Chandigarh, IN.
+                        </p>
                         <div className="flex justify-center">
                             <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                                 Join NOW!
                             </button>
-                           
                         </div>
                     </div>
                 </div>
